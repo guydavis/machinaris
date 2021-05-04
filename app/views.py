6c5d567a296ad1c48c9e98dd9e5e696a558ca4c8
Guy Davis <guydavis.ca@gmail.com>
2021-05-04 11:41:15 -0600

    DEV

--- app/views.py
+++ app/views.py
@@ -1,35 +1,6 @@
-# Flask modules
-from flask   import render_template, request
-from jinja2  import TemplateNotFound
+from flask import Flask
+app = Flask(__name__)
 
-# App modules
-from app import app
-
-# App main route + generic routing
-@app.route('/', defaults={'path': 'index.html'})
-@app.route('/<path>')
-def index(path):
-    try:
-
-        # Detect the current page
-        segment = get_segment( request )
-
-        # Serve the file (if exists) from app/templates/FILE.html
-        return render_template( path, segment=segment )
-    
-    except TemplateNotFound:
-        return render_template('page-404.html'), 404
-
-def get_segment( request ): 
-
-    try:
-
-        segment = request.path.split('/')[-1]
-
-        if segment == '':
-            segment = 'index'
-
-        return segment    
-
-    except:
-        return None  
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
