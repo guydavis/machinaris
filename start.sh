9179fabc2fb22da267d45ea4d5181c24269740d6
Guy Davis <guydavis.ca@gmail.com>
2021-05-09 09:49:12 -0600

    Enable push to DockerHub

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@
 echo 'Configuring Plotman...'
 mkdir -p /root/.chia/plotman
 mkdir -p /root/.chia/logs
-cp /machinaris/plotman.sample.yaml /root/.chia/plotman/plotman.yaml
+cp -n /machinaris/plotman.sample.yaml /root/.chia/plotman/plotman.yaml
 
 echo 'Starting Machinaris web server...'
 if [ $FLASK_ENV == "development" ];
 then
-    python3 -m flask run --host=0.0.0.0 > /root/.chia/logs/webui.log 2>&1 &
+    /chia-blockchain/venv/bin/python3 -m flask run --host=0.0.0.0 > /root/.chia/logs/webui.log 2>&1 &
 else
-    gunicorn --bind 0.0.0.0:5000 run:app > /root/.chia/logs/webui.log 2>&1 &
+    /chia-blockchain/venv/bin/gunicorn --bind 0.0.0.0:5000 run:app > /root/.chia/logs/webui.log 2>&1 &
 fi
