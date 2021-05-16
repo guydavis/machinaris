439f54cf1280db9fe666435692bddce0584c990e
Guy Davis <guydavis.ca@gmail.com>
2021-05-15 19:47:08 -0600

    Disable key generate on missing file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,10 +14,10 @@ chia init
 if [[ ${keys} == "generate" ]]; then
   echo "to use your own keys pass them as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
   chia keys generate
-elif [[ ! -f ${keys} ]]; then
-  echo "no such keys file yet, so probably first launch, generating keys and storing mnemonic now..."
-  chia keys generate
-  chia keys show --show-mnemonic-seed | tail -n 1 > ${keys}
+#elif [[ ! -f ${keys} ]]; then
+#  echo "no such keys file yet, so probably first launch, generating keys and storing mnemonic now..."
+#  chia keys generate
+#  chia keys show --show-mnemonic-seed | tail -n 1 > ${keys}
 else
   chia keys add -f ${keys}
 fi
