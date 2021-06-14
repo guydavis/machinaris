5edc6129d75c7fa951cc3d0d412b1aabc4e376d5
Guy Davis <guydavis.ca@gmail.com>
2021-06-13 22:26:22 -0600

    Latest on plot control.

---
+++ scripts/start-api-dev.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+#
+# Launch WebUI in DEV mode from within container.  
+# NOT IN GIT.  Runs in foreground of shell.
+#
+
+echo 'Starting Machinaris...'
+mkdir -p /root/.chia/machinaris/logs
+cd /machinaris-dev
+LOG_LEVEL='debug'
+RELOAD='--reload'
+
+# To enable SSL, use the Chia self-signed cert
+    #--certfile=/root/.chia/mainnet/config/ssl/ca/chia_ca.crt \
+    #--keyfile=/root/.chia/mainnet/config/ssl/ca/chia_ca.key \
+
+/chia-blockchain/venv/bin/gunicorn ${RELOAD} \
+    --bind 0.0.0.0:8927 --timeout 90 \
+    --log-level=${LOG_LEVEL} \
+    api:app
---
+++ scripts/start-web-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+#
+# Launch WebUI in DEV mode from within container.  
+# NOT IN GIT.  Runs in foreground of shell.
+#
+
+echo 'Starting Machinaris...'
+mkdir -p /root/.chia/machinaris/logs
+cd /machinaris-dev
+
+/chia-blockchain/venv/bin/gunicorn \
+    --reload \
+    --bind 0.0.0.0:8926 \
+    --timeout 90 \
+    --log-level=debug \
+    web:app
