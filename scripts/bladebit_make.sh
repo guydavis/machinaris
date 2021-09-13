e24083811dbaa6989a2ce820becc5ea57703c777
Guy Davis <guydavis.ca@gmail.com>
2021-09-13 08:33:46 -0600

    Offset of '+dev'.

--- scripts/bladebit_make.sh
+++ scripts/bladebit_make.sh
@@ -9,18 +9,20 @@ if [ ! -f /usr/bin/bladebit ]; then
     # Build the code, previously cloned when image was built on Github build servers
     cd /bladebit
     ./build-bls 2>&1 >/tmp/bladebit_make.log
+    make clean 2>&1 >>/tmp/bladebit_make.log
     arch_name="$(uname -m)"
     echo "arch_name=${arch_name}"
     if [ "${arch_name}" = "x86_64" ]; then
         echo "Building bladebit on x86_64..."
-        make clean && make -j$(nproc --all) 2>&1 >>/tmp/bladebit_make.log
+        make -j$(nproc --all) 2>&1 >>/tmp/bladebit_make.log
         ln -s /bladebit/.bin/release/bladebit /usr/bin/bladebit 
     elif [ "${arch_name}" = "arm64" ]; then
         echo "Building bladebit on arm64..."
-        make clean && make -j$(nproc --all) CONFIG=release.arm 2>&1 >>/tmp/bladebit_make.log
+        make -j$(nproc --all) CONFIG=release.arm 2>&1 >>/tmp/bladebit_make.log
         ln -s /bladebit/.bin/release.arm/bladebit /usr/bin/bladebit
     else
         echo "Building bladebit failed -> Unknown architecture: ${arch_name}"
         exit 1
     fi
+    echo "Bladebit version: "`bladebit --version`
 fi
