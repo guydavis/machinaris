5b8d25d8bbd3d868e302d2341dbc7e6e8998803e
Guy Davis <guydavis.ca@gmail.com>
2021-09-13 16:48:12 -0600

    Test Unraid log workaround.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 #  Configure and start plotting and farming services.
 #
 
+# Print a wide line to ensure Unraid's Docker Logs view doesn't truncate the left-side of our logs
+echo "Welcome! Machinaris is an easy-to-use WebUI for Chia plotting and farming. Includes Chia, Plotman, MadMax, Chiadog, Bladebit and Flax in a single Docker image."
+
 # Always launch Chia - required blockchain
 /usr/bin/bash /machinaris/scripts/chia_launch.sh
 
