6814686d0d8c30abde421905d4fa629b470867dd
Guy Davis <guydavis.ca@gmail.com>
2021-09-14 18:09:32 -0600

    Cleanups for RaspPi launching.

--- scripts/bladebit_make.sh
+++ scripts/bladebit_make.sh
@@ -6,23 +6,25 @@
 #
 
 if [ ! -f /usr/bin/bladebit ]; then
-    # Build the code, previously cloned when image was built on Github build servers
     cd /bladebit
-    ./build-bls
-    make clean
+    # Build the code, previously cloned when image was built on Github build servers
     arch_name="$(uname -m)"
     echo "arch_name=${arch_name}"
     if [ "${arch_name}" = "x86_64" ]; then
         echo "Building bladebit on x86_64..."
+        ./build-bls
+        make clean
         make -j$(nproc --all) 
         ln -s /bladebit/.bin/release/bladebit /usr/bin/bladebit
+        echo "Bladebit version: "`bladebit --version`
     elif [ "${arch_name}" = "arm64" ]; then
         echo "Building bladebit on arm64..."
+        ./build-bls
+        make clean
         make -j$(nproc --all) CONFIG=release.arm 
         ln -s /bladebit/.bin/release.arm/bladebit /usr/bin/bladebit
+        echo "Bladebit version: "`bladebit --version`
     else
-        echo "Building bladebit failed -> Unknown architecture: ${arch_name}"
-        exit 1
+        echo "Building bladebit skipped -> unsupported architecture: ${arch_name}"
     fi
-    echo "Bladebit version: "`bladebit --version`
 fi
