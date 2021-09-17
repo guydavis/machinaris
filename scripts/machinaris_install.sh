51b6b18674a3f5ec4d7194711d2c8e5c22dd1c04
Guy Davis <guydavis.ca@gmail.com>
2021-09-16 21:43:41 -0600

    Path to entrypoint.sh

--- scripts/machinaris_install.sh
+++ scripts/machinaris_install.sh
@@ -9,6 +9,6 @@ cd /chia-blockchain
 
 venv/bin/pip3 install -r /machinaris/docker/requirements.txt
 
-cp -f /machinaris/entrypoint.sh /chia-blockchain/ 
+cp -f /machinaris/docker/entrypoint.sh /chia-blockchain/ 
 
 chmod 755 /machinaris/scripts/* /chia-blockchain/entrypoint.sh
