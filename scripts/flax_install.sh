3235f2c725d7fcd9c20df9bbfb56b93c38a63c2b
Guy Davis <guydavis.ca@gmail.com>
2021-09-18 22:17:23 -0600

    Trying linkage.

--- scripts/flax_install.sh
+++ scripts/flax_install.sh
@@ -4,9 +4,11 @@
 #
 FLAX_BRANCH=$1
 
+
 if [ -z ${FLAX_BRANCH} ]; then
 	echo 'Skipping Flax install as not requested.'
 else
+	cd /
 	rm -rf /root/.cache
 	git clone --branch ${FLAX_BRANCH} --single-branch https://github.com/Flax-Network/flax-blockchain.git /flax-blockchain \
 		&& cd /flax-blockchain \
@@ -14,7 +16,13 @@ else
 		&& chmod +x install.sh \
 		&& /usr/bin/sh ./install.sh
 
+	echo "FLAX_FINISHED: checking root directory"
+	ls -al /
+
 	if [ ! -d /chia-blockchain]; then
+		echo "Trying to link chia-blockchain"
 		ln -s /flax-blockchain /chia-blockchain
 	fi
+
+	ls -al /
 fi
