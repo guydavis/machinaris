955e87d8fe094466b376f717496c8cca093611be
Guy Davis <guydavis.ca@gmail.com>
2021-09-26 09:52:08 -0600

    NChain install.

--- scripts/nchain_install.sh
+++ scripts/nchain_install.sh
@@ -8,9 +8,9 @@ NCHAIN_BRANCH=$1
 if [ -z ${NCHAIN_BRANCH} ]; then
 	echo 'Skipping NChain install as not requested.'
 else
-	git clone --branch ${NCHAIN_BRANCH} https://gitee.com/ext9/ext9-blockchain.git  --recurse-submodules
-	cd ext9-blockchain
-	sh install.sh
-	. ./activate
-	chia start node
+	git clone --branch ${NCHAIN_BRANCH} --recurse-submodules https://gitee.com/ext9/ext9-blockchain.git /ext9-blockchain \
+		&& cd /ext9-blockchain \
+		&& git submodule update --init mozilla-ca \
+		&& chmod +x install.sh \
+		&& /usr/bin/sh ./install.sh
 fi
