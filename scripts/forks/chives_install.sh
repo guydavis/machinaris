985d0f4161210ebad2882ad5f83ca445fd210fa8
Guy Davis <guydavis.ca@gmail.com>
2021-09-26 19:44:04 -0600

    Git fixes

--- scripts/forks/chives_install.sh
+++ scripts/forks/chives_install.sh
@@ -9,6 +9,7 @@ if [ -z ${CHIVES_BRANCH} ]; then
 	echo 'Skipping Chives install as not requested.'
 else
 	git clone --branch ${CHIVES_BRANCH} --recurse-submodules https://github.com/HiveProject2021/chives-blockchain.git /chives-blockchain \
+		&& cd /chives-blockchain \
 		&& git submodule update --init mozilla-ca \
 		&& chmod +x install.sh \
 		&& /usr/bin/sh ./install.sh
--- scripts/forks/hddcoin_install.sh
+++ scripts/forks/hddcoin_install.sh
@@ -8,7 +8,7 @@ HDDCOIN_BRANCH=$1
 if [ -z ${HDDCOIN_BRANCH} ]; then
 	echo 'Skipping HDDCoin install as not requested.'
 else
-	git clone --branch ${HDDCOIN_BRANCH} --recurse-submodules git clone https://github.com/HDDcoin-Network/hddcoin-blockchain.git /hddcoin-blockchain \
+	git clone --branch ${HDDCOIN_BRANCH} --recurse-submodules https://github.com/HDDcoin-Network/hddcoin-blockchain.git /hddcoin-blockchain \
 		&& cd /hddcoin-blockchain \
 		&& git submodule update --init mozilla-ca \
 		&& chmod +x install.sh \
