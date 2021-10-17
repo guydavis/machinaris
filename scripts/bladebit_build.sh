9bedd59855ef0ba72b1c1ceff9b9e13f8d9fe6e9
Guy Davis <guydavis.ca@gmail.com>
2021-10-17 08:23:47 -0600

    Bladebit build on fullnodes & plotters.

--- scripts/bladebit_build.sh
+++ scripts/bladebit_build.sh
@@ -6,10 +6,20 @@
 # runs on. 
 #
 
-cd /bladebit
-mkdir -p build && cd build
-cmake ..
-cmake --build . --target bladebit --config Release
-ln -s /bladebit/build/bladebit /usr/bin/bladebit
-cd /
-echo "Bladebit version: "`bladebit --version`
+if [[ ${mode} == 'fullnode' ]] || [[ ${mode} =~ ^plotter.* ]]; then
+    if [ ! -f /usr/bin/bladebit ]; then
+        # Build the code, previously cloned when image was built on Github build servers
+        arch_name="$(uname -m)"
+        echo "arch_name=${arch_name}"
+        if [[ "${arch_name}" = "x86_64" ]] || [[ "${arch_name}" = "arm64" ]]; then
+            cd /bladebit && echo "Building bladebit on ${arch_name}..."
+            mkdir -p build && cd build
+            cmake ..
+            cmake --build . --target bladebit --config Release
+            ln -s /bladebit/build/bladebit /usr/bin/bladebit
+            cd / && echo "Bladebit version: "`bladebit --version`
+        else
+            echo "Building bladebit skipped -> unsupported architecture: ${arch_name}"
+        fi
+    fi
+fi
