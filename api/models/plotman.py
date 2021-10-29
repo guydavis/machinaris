d8b281f687e752e7497b8fabcf1483955ffc13a2
Guy Davis <guydavis.ca@gmail.com>
2021-10-29 15:04:57 -0600

    Handle bladebit status line from plotman.

--- api/models/plotman.py
+++ api/models/plotman.py
@@ -11,11 +11,13 @@ class PlottingSummary:
     def __init__(self, cli_stdout, plotman_pid):
         self.rows = []
         for line in cli_stdout:
-            if "plot id" in line.strip(): # The header row
+            if not line.strip() or line.startswith("Total jobs") or line.startswith("Updated at") or line.startswith("Jobs in"):
+                pass
+            elif "plot id" in line.strip(): # The header row
                 self.columns = line.replace('plot id', 'plot_id').strip().split()
                 # Plotman has two columns both named 'tmp' so change the 2nd one to 'size'
                 self.columns[7] = 'size'
-            elif re.match(r'^(\w){8}\s.*$', line.strip()): # A plotting row, so create as dictionary
+            elif re.match(r'^(\w){8}\s.*$', line.strip()): # A regular plotting row, so create as dictionary
                 row = {}
                 values = line.split()
                 i = 0
@@ -23,6 +25,18 @@ class PlottingSummary:
                     row[self.columns[i]] = values[i]
                     i += 1
                 self.rows.append(row)
+            elif re.match(r'^(\w){7}\s.*$', line.strip()): # A bladebit plotting row with no 'tmp' value col
+                row = {}
+                values = line.split()
+                values.insert(3, '-')  # For tmp dir location, bladebit doesn't use it.
+                app.logger.info(values)
+                i = 0
+                for i in range(len(self.columns)):
+                    row[self.columns[i]] = values[i]
+                    i += 1
+                self.rows.append(row)
+            else:
+                app.logger.info("Unkown Plotman status line: {0}".format(line))
         self.calc_status()
         if plotman_pid:
             self.plotman_running = True
