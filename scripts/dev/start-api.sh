6cedb8b4e068bd4c4df16d3435a83494660c0f66
Guy Davis <guydavis.ca@gmail.com>
2021-11-15 13:48:21 -0700

    Another attempt at parsing bladebit plotman lines.

--- scripts/dev/start-api.sh
+++ scripts/dev/start-api.sh
@@ -22,7 +22,7 @@ fi
     #--keyfile=/root/.chia/mainnet/config/ssl/ca/chia_ca.key \
 
 /chia-blockchain/venv/bin/gunicorn ${RELOAD} \
-    --bind 0.0.0.0:8927 \
+    --bind 0.0.0.0:${worker_api_port} \
     --timeout 90 \
     --log-level=${LOG_LEVEL} \
     --workers=2 \
