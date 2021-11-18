318be97d1fa79f9829ea7738b105e8e8af21dbe4
Guy Davis <guydavis.ca@gmail.com>
2021-11-17 19:38:08 -0700

    Forktools multiproc patching.

--- scripts/forktools_setup.sh
+++ scripts/forktools_setup.sh
@@ -3,13 +3,23 @@
 # Installs forktools - https://github.com/Qwinn1/forktools
 #
 
+# On 2021-11-17
+HASH=65bf10c63e701de732b61b24fafeab03e6d1e695
+
 if [[ ${mode} == 'fullnode' ]] || [[ ${mode} =~ "harvester" ]]; then
-    if [[ ! -d /forktools ]]; then
-		cd /
+	cd /root/.chia
+    if [[ ! -d /root/.chia/forktools ]]; then  # fresh install
 		export FORKTOOLSBLOCKCHAINDIRS=/
 		git clone https://github.com/Qwinn1/forktools
-		cd forktools
-		bash installft.sh
-		source ~/.bashrc
+	fi
+	cd forktools
+	git checkout $HASH
+	chmod +x $PWD/install*.sh
+	$PWD/install2ft.sh
+	source ~/.bashrc
+
+	# Now multiproc patch fullnodes to limit memory usage
+	if [[ ${mode} == 'fullnode' ]]; then
+		forkpatch all -multiproc
 	fi
 fi
