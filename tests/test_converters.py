5ab99f02becaa4a9e8154b501cc8b769bd24d94c
Guy Davis <guydavis.ca@gmail.com>
2021-11-20 12:49:22 -0700

    Unit tests.

---
+++ tests/test_converters.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import unittest
+
+from common.utils import converters
+
+class TestRoundBalance(unittest.TestCase):
+
+    def test_zero(self):
+        data = 0
+        result = converters.round_balance(data)
+        self.assertEqual(result, "0.0")
+
+    def test_neg_one(self):
+        data = -1
+        result = converters.round_balance(data)
+        self.assertEqual(result, "-1.0")
+
+    def test_plus_one(self):
+        data = 1
+        result = converters.round_balance(data)
+        self.assertEqual(result, "1.0")
+
+    def test_two_million(self):
+        data = 2000000
+        result = converters.round_balance(data)
+        self.assertEqual(result, "2000000")
