1c56c1a4869367d7ec01767874297dc498a51e50
Guy Davis <guydavis.ca@gmail.com>
2021-11-20 14:51:00 -0700

    Adding unit testing with pytest.

--- tests/README.md
+++ tests/README.md
@@ -1,1 +1,5 @@
-See: https://testdriven.io/blog/flask-pytest/
+To run tests:
+
+    $ pytests
+
+For functional testing, see: https://testdriven.io/blog/flask-pytest/
--- tests/unit/common/utils/test_converters.py
+++ tests/unit/common/utils/test_converters.py
@@ -10,7 +10,7 @@ class TestRoundBalance(unittest.TestCase):
     def test_zero(self):
         data = 0
         result = converters.round_balance(data)
-        self.assertEqual(result, "0.0")
+        self.assertEqual(result, "0")
 
     def test_neg_one(self):
         data = -1
@@ -22,7 +22,47 @@ class TestRoundBalance(unittest.TestCase):
         result = converters.round_balance(data)
         self.assertEqual(result, "1.0")
 
-    def test_two_million(self):
+    def test_tenths(self):
+        data = 0.2
+        result = converters.round_balance(data)
+        self.assertEqual(result, "0.2")
+
+    def test_hundredths(self):
+        data = 0.02
+        result = converters.round_balance(data)
+        self.assertEqual(result, "0.02")
+
+    def test_thousandths(self):
+        data = 0.002
+        result = converters.round_balance(data)
+        self.assertEqual(result, "0.002")
+    
+    def test_tenthousandths(self):
+        data = 0.0002
+        result = converters.round_balance(data)
+        self.assertEqual(result, "0.0002")
+    
+    def test_millionths(self):
+        data = 0.000002
+        result = converters.round_balance(data)
+        self.assertEqual(result, "0.0")
+
+    def test_singledigit(self):
+        data = 2
+        result = converters.round_balance(data)
+        self.assertEqual(result, "2.0")
+
+    def test_thousand(self):
+        data = 2000
+        result = converters.round_balance(data)
+        self.assertEqual(result, "2,000")
+
+    def test_tenthousand(self):
+        data = 20000
+        result = converters.round_balance(data)
+        self.assertEqual(result, "20,000")
+
+    def test_million(self):
         data = 2000000
         result = converters.round_balance(data)
-        self.assertEqual(result, "2000000")
+        self.assertEqual(result, "2,000,000")
