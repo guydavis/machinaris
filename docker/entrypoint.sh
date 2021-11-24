730668afb0e28805e88ea87270ccc3cdeeef42ea
Guy Davis <guydavis.ca@gmail.com>
2021-11-24 16:50:04 -0700

    More guards on launching.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -25,40 +25,15 @@ if [[ -z "${worker_address}" ]]; then
   exit 1
 fi
 
+# Refuse to run if Portainer launched containers out of order and created a directory for mnemonic.txt
+if [[ -d /root/.chia/mnemonic.txt ]]; then
+  echo "Portainer (or similar) seems to have launched a fork container before the main Machinaris container on first run."
+  echo "Now we have a mnemonic.txt directory, instead of an empty file.  Please correct and start only machinaris container first."
+  exit 1
+fi
+
 # Warn if non-standard worker_api_port is being used, likely default value they did not override properly
-if [[ "${blockchains}"  == "chia" && "${worker_api_port}" != '8927']]; then
-  echo "Chia worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8927?"
-fi
-if [[ "${blockchains}"  == "cactus" && "${worker_api_port}" != '8936']]; then
-  echo "Cactus worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8936?"
-fi
-if [[ "${blockchains}"  == "chives" && "${worker_api_port}" != '8931']]; then
-  echo "Chives worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8931?"
-fi
-if [[ "${blockchains}"  == "cryptodoge" && "${worker_api_port}" != '8937']]; then
-  echo "Cryptodoge worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8937?"
-fi
-if [[ "${blockchains}"  == "flax" && "${worker_api_port}" != '8928']]; then
-  echo "Flax worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8928?"
-fi
-if [[ "${blockchains}"  == "flora" && "${worker_api_port}" != '8932']]; then
-  echo "Flora worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8932?"
-fi
-if [[ "${blockchains}"  == "hddcoin" && "${worker_api_port}" != '8930']]; then
-  echo "HDDCoin worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8930?"
-fi
-if [[ "${blockchains}"  == "maize" && "${worker_api_port}" != '8933']]; then
-  echo "Maize worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8933?"
-fi
-if [[ "${blockchains}"  == "nchain" && "${worker_api_port}" != '8929']]; then
-  echo "N-Chain worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8936?"
-fi  
-if [[ "${blockchains}"  == "staicoin" && "${worker_api_port}" != '8934']]; then
-  echo "Staicoin worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8934?"
-fi
-if [[ "${blockchains}"  == "stor" && "${worker_api_port}" != '8935']]; then
-  echo "Stor worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8935?"
-fi  
+/usr/bin/bash /machinaris/scripts/worker_port_warning.sh 
 
 # If on Windows, possibly mount SMB remote shares as defined in 'remote_shares' env var
 /usr/bin/bash /machinaris/scripts/mount_remote_shares.sh > /tmp/mount_remote_shares.log
---
+++ scripts/worker_port_warning.sh
@@ -0,0 +1,34 @@
+# Warn if non-standard worker_api_port is being used, likely default value they did not override properly
+if [[ "${blockchains}"  == "chia" && "${worker_api_port}" != '8927' ]]; then
+  echo "Chia worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8927?"
+fi
+if [[ "${blockchains}"  == "cactus" && "${worker_api_port}" != '8936' ]]; then
+  echo "Cactus worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8936?"
+fi
+if [[ "${blockchains}"  == "chives" && "${worker_api_port}" != '8931' ]]; then
+  echo "Chives worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8931?"
+fi
+if [[ "${blockchains}"  == "cryptodoge" && "${worker_api_port}" != '8937' ]]; then
+  echo "Cryptodoge worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8937?"
+fi
+if [[ "${blockchains}"  == "flax" && "${worker_api_port}" != '8928' ]]; then
+  echo "Flax worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8928?"
+fi
+if [[ "${blockchains}"  == "flora" && "${worker_api_port}" != '8932' ]]; then
+  echo "Flora worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8932?"
+fi
+if [[ "${blockchains}"  == "hddcoin" && "${worker_api_port}" != '8930' ]]; then
+  echo "HDDCoin worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8930?"
+fi
+if [[ "${blockchains}"  == "maize" && "${worker_api_port}" != '8933' ]]; then
+  echo "Maize worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8933?"
+fi
+if [[ "${blockchains}"  == "nchain" && "${worker_api_port}" != '8929']]; then
+  echo "N-Chain worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8936?"
+fi  
+if [[ "${blockchains}"  == "staicoin" && "${worker_api_port}" != '8934' ]]; then
+  echo "Staicoin worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8934?"
+fi
+if [[ "${blockchains}"  == "stor" && "${worker_api_port}" != '8935' ]]; then
+  echo "Stor worker with non-standard worker_api_port of ${worker_api_port} found.  Did you mean to use 8935?"
+fi 
