45b171b20af7a6020cfc4c3128b8328e8f0b5c17
Guy Davis <guydavis.ca@gmail.com>
2021-12-05 07:28:45 -0700

    Update to latest BTCGreen

--- scripts/forks/btcgreen_install.sh
+++ scripts/forks/btcgreen_install.sh
@@ -4,8 +4,8 @@
 #
 
 BTCGREEN_BRANCH=$1
-# On 2021-12-01
-HASH=d1c7f183aa44ca835ec621c96ae6debd703d200b
+# On 2021-12-05
+HASH=263e39a22a0415028b5989511302d351d7158b38
 
 if [ -z ${BTCGREEN_BRANCH} ]; then
 	echo 'Skipping BTCGreen install as not requested.'
