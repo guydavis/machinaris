801f67162ed5df963666c97587a164153a6fa0e8
Guy Davis <guydavis.ca@gmail.com>
2021-12-28 21:15:06 -0700

    Log and config paths on volume.

--- scripts/forks/mmx_launch.sh
+++ scripts/forks/mmx_launch.sh
@@ -4,9 +4,13 @@
 #
 
 cd /mmx-node
+
+rm -rf ./logs
 mkdir -p /root/.chia/mmx/logs
 ln -s /root/.chia/mmx/logs logs
+
+rm -rf ./logs
 mkdir -p /root/.chia/mmx/config/local
 ln -s /root/.chia/mmx/config config
 
-./run_node.sh
+./run_node.sh >/root/.chia/mmx/logs/mmx_node.log 2>&1 &
