b6f644a4d6f183cb542795d9d08e5c0d8ab367f7
Guy Davis <guydavis.ca@gmail.com>
2021-12-29 10:57:51 -0700

    More fixes for Staicoin renaming.

--- scripts/forks/staicoin_launch.sh
+++ scripts/forks/staicoin_launch.sh
@@ -9,20 +9,18 @@ cd /staicoin-blockchain
 
 # Only the /root/.chia folder is volume-mounted so store staicoin within
 mkdir -p /root/.chia/staicoin
-rm -f /root/.staicoin
-ln -s /root/.chia/staicoin /root/.staicoin 
 rm -f /root/.stai
 ln -s /root/.chia/staicoin /root/.stai
 
-mkdir -p /root/.staicoin/mainnet/log
-stai init >> /root/.staicoin/mainnet/log/init.log 2>&1 
+mkdir -p /root/.stai/mainnet/log
+stai init >> /root/.stai/mainnet/log/init.log 2>&1 
 
 if [[ "${blockchain_db_download}" == 'true' ]] \
   && [[ "${mode}" == 'fullnode' ]] \
-  && [[ ! -f /root/.staicoin/mainnet/db/blockchain_v1_mainnet.sqlite ]]; then
+  && [[ ! -f /root/.stai/mainnet/db/blockchain_v1_mainnet.sqlite ]]; then
   echo "Downloading Staicoin blockchain DB (many GBs in size) on first launch..."
   echo "Please be patient as takes minutes now, but saves days of syncing time later."
-  mkdir -p /root/.staicoin/mainnet/db/ && cd /root/.staicoin/mainnet/db/
+  mkdir -p /root/.stai/mainnet/db/ && cd /root/.stai/mainnet/db/
   # Latest Blockchain DB download from direct from https://stai.global
   curl -skJLO https://stai.global/database/staiblockchain.rar
   unrar staiblockchain.rar 
@@ -31,12 +29,12 @@ if [[ "${blockchain_db_download}" == 'true' ]] \
 fi
 
 echo 'Configuring Staicoin...'
-if [ -f /root/.staicoin/mainnet/config/config.yaml ]; then
-  sed -i 's/log_stdout: true/log_stdout: false/g' /root/.staicoin/mainnet/config/config.yaml
-  sed -i 's/log_level: WARNING/log_level: INFO/g' /root/.staicoin/mainnet/config/config.yaml
-  sed -i 's/localhost/127.0.0.1/g' /root/.staicoin/mainnet/config/config.yaml
+if [ -f /root/.stai/mainnet/config/config.yaml ]; then
+  sed -i 's/log_stdout: true/log_stdout: false/g' /root/.stai/mainnet/config/config.yaml
+  sed -i 's/log_level: WARNING/log_level: INFO/g' /root/.stai/mainnet/config/config.yaml
+  sed -i 's/localhost/127.0.0.1/g' /root/.stai/mainnet/config/config.yaml
   # Fix for their renaming from staicoin to stai in December 2021
-  sed -i 's/staicoin_ssl_ca/stai_ssl_ca/g' /root/.staicoin/mainnet/config/config.yaml
+  sed -i 's/staicoin_ssl_ca/stai_ssl_ca/g' /root/.stai/mainnet/config/config.yaml
 fi
 
 # Loop over provided list of key paths
@@ -54,7 +52,7 @@ for p in ${plots_dir//:/ }; do
     stai plots add -d ${p}
 done
 
-chmod 755 -R /root/.staicoin/mainnet/config/ssl/ &> /dev/null
+chmod 755 -R /root/.stai/mainnet/config/ssl/ &> /dev/null
 stai init --fix-ssl-permissions > /dev/null 
 
 # Start services based on mode selected. Default is 'fullnode'
@@ -71,7 +69,7 @@ if [[ ${mode} == 'fullnode' ]]; then
   done
   stai start farmer
 elif [[ ${mode} =~ ^farmer.* ]]; then
-  if [ ! -f ~/.staicoin/mainnet/config/ssl/wallet/public_wallet.key ]; then
+  if [ ! -f ~/.stai/mainnet/config/ssl/wallet/public_wallet.key ]; then
     echo "No wallet key found, so not starting farming services.  Please add your Chia mnemonic.txt to the ~/.machinaris/ folder and restart."
   else
     stai start farmer-only
@@ -81,22 +79,22 @@ elif [[ ${mode} =~ ^harvester.* ]]; then
     echo "A farmer peer address and port are required."
     exit
   else
-    if [ ! -f /root/.staicoin/farmer_ca/private_ca.crt ]; then
-      mkdir -p /root/.staicoin/farmer_ca
+    if [ ! -f /root/.stai/farmer_ca/private_ca.crt ]; then
+      mkdir -p /root/.stai/farmer_ca
       response=$(curl --write-out '%{http_code}' --silent http://${farmer_address}:8934/certificates/?type=stai --output /tmp/certs.zip)
       if [ $response == '200' ]; then
-        unzip /tmp/certs.zip -d /root/.staicoin/farmer_ca
+        unzip /tmp/certs.zip -d /root/.stai/farmer_ca
       else
-        echo "Certificates response of ${response} from http://${farmer_address}:8934/certificates/?type=staicoin.  Try clicking 'New Worker' button on 'Workers' page first."
+        echo "Certificates response of ${response} from http://${farmer_address}:8934/certificates/?type=stai.  Try clicking 'New Worker' button on 'Workers' page first."
       fi
       rm -f /tmp/certs.zip 
     fi
-    if [ -f /root/.staicoin/farmer_ca/private_ca.crt ]; then
-      stai init -c /root/.staicoin/farmer_ca 2>&1 > /root/.staicoin/mainnet/log/init.log
-      chmod 755 -R /root/.staicoin/mainnet/config/ssl/ &> /dev/null
+    if [ -f /root/.stai/farmer_ca/private_ca.crt ]; then
+      stai init -c /root/.stai/farmer_ca 2>&1 > /root/.stai/mainnet/log/init.log
+      chmod 755 -R /root/.stai/mainnet/config/ssl/ &> /dev/null
       stai init --fix-ssl-permissions > /dev/null 
     else
-      echo "Did not find your farmer's certificates within /root/.staicoin/farmer_ca."
+      echo "Did not find your farmer's certificates within /root/.stai/farmer_ca."
       echo "See: https://github.com/guydavis/machinaris/wiki/Workers#harvester"
     fi
     echo "Configuring farmer peer at ${farmer_address}:${farmer_port}"
