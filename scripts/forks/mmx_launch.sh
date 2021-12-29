79a743fefb8adab157e81d187b34a107ee2f72f3
Guy Davis <guydavis.ca@gmail.com>
2021-12-29 14:25:39 -0700

    Hold MMX storage on mounted volume.

--- scripts/forks/mmx_launch.sh
+++ scripts/forks/mmx_launch.sh
@@ -16,4 +16,11 @@ ln -s /root/.chia/mmx/config config
 echo "Current state of the $(pwd) folder:"
 ls -al .
 
+tee /root/.chia/mmx/config/local/Node.json >/dev/null <<EOF
+{
+	"opencl_device": 0,
+	"storage_path": "/root/.chia/mmx/"
+}
+EOF
+
 ./run_node.sh >/root/.chia/mmx/logs/mmx_node.log 2>&1 &
