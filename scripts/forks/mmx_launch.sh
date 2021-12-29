2dd1269aa1c733934f7d3e1e77757328413ed139
Guy Davis <guydavis.ca@gmail.com>
2021-12-29 16:13:48 -0700

    More fixes for MMX storage.

--- scripts/forks/mmx_launch.sh
+++ scripts/forks/mmx_launch.sh
@@ -9,18 +9,28 @@ rm -rf ./logs
 mkdir -p /root/.chia/mmx/logs
 ln -s /root/.chia/mmx/logs logs
 
-rm -rf ./config
-mkdir -p /root/.chia/mmx/config/local
-ln -s /root/.chia/mmx/config config
+
+if [ -d /root/.chia/mmx/config/local ]; then
+	mv /root/.chia/mmx/config/local config/backup
+	git checkout -- config/
+	./update.sh
+	mv config/backup /root/.chia/mmx/config/local
+	mv ./config /root/.chia/mmx/config
+	ln -s /root/.chia/mmx/config config
+else
+	mv ./config /root/.chia/mmx/config
+	mkdir -p /root/.chia/mmx/config/local
+	tee /root/.chia/mmx/config/local/Node.json >/dev/null <<EOF
+	{
+		"opencl_device": 0,
+		"storage_path": "/root/.chia/mmx/"
+	}
+EOF
+	ln -s /root/.chia/mmx/config config
+fi
+cp 
 
 echo "Current state of the $(pwd) folder:"
 ls -al .
-
-tee /root/.chia/mmx/config/local/Node.json >/dev/null <<EOF
-{
-	"opencl_device": 0,
-	"storage_path": "/root/.chia/mmx/"
-}
-EOF
 
 ./run_node.sh >/root/.chia/mmx/logs/mmx_node.log 2>&1 &
