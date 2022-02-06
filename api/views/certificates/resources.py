ff9ae0702a7bf7e88d2951fe4ab585447f828412
Guy Davis <guydavis.ca@gmail.com>
2022-02-05 21:48:57 -0700

    Fix certs path lookup for Silicoin.

--- api/views/certificates/resources.py
+++ api/views/certificates/resources.py
@@ -57,9 +57,6 @@ class Certificates(MethodView):
         elif type == "maize":
             blockchain = "maize"
             dir = "/root/.maize/mainnet/config/ssl/ca"
-        elif type == "silicoin":
-            blockchain = "silicoin"
-            dir = "/root/.silicoin/mainnet/config/ssl/ca"
         elif type == "shibgreen":
             blockchain = "shibgreen"
             dir = "/root/.shibgreen/mainnet/config/ssl/ca"
