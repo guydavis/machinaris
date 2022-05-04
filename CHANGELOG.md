1dea32e67cebc0d90a7f7444b03da66fa7e9cd42
Guy Davis <guydavis.ca@gmail.com>
2022-05-04 16:09:58 -0600

    Drive failure alerts thru our enhanced Chiadog.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,7 @@
 All notable changes to this project will be documented in this file. The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [0.7.2] - 2022-05-?
- - Drives monitoring page - allow [overrides of smartctl commands](https://github.com/guydavis/machinaris/wiki/Drives#override-command) for special hardware.
+ - Drives monitoring page - allow [overrides of smartctl commands](https://github.com/guydavis/machinaris/wiki/Drives#override-command) for special hardware. Drive failure alerts thru our enhanced Chiadog.
  - [Chia](https://github.com/Chia-Network/chia-blockchain/releases/tag/1.3.3) - v1.3.3 release
  - [Chia](https://github.com/Chia-Network/chia-blockchain/releases/tag/1.3.4) - v1.3.4 release - update full node before harvesters
  - [Staicoin](https://github.com/STATION-I/stai-blockchain) - v1.2 release - careful, bugs reported on their Discord!
--- scripts/chiadog_setup.sh
+++ scripts/chiadog_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/env bash
 #
-# Installs Chiadog for log monitoring and alerting
+# Installs Chiadog for log monitoring and alerting,
+# using an enhanced and supported fork of the original project.
 #
 
-CHIADOG_BRANCH=dev
+CHIADOG_BRANCH=main
 
 if [[ (${mode} == 'fullnode' || ${mode} =~ "harvester") && ${blockchains} != 'mmx' ]]; then
     if [[ ! -f /chiadog/main.py ]]; then
@@ -11,12 +12,6 @@ if [[ (${mode} == 'fullnode' || ${mode} =~ "harvester") && ${blockchains} != 'mm
         cd /
         git clone --branch ${CHIADOG_BRANCH} https://github.com/guydavis/chiadog.git
         cd /chia-blockchain/
-        # Chia-blockchain needs PyYAML=5.4.1 but Chiadog wants exactly 5.4
-        sed -i 's/==5.4/~=5.4/g' /chiadog/requirements.txt
-        # Also, as per Chiadog integrations page, the MQTT integration needs
-        # https://github.com/martomi/chiadog/blob/main/INTEGRATIONS.md
-        printf "\npaho-mqtt" >> /chiadog/requirements.txt
-        # Now install Chiadog python dependencies
         venv/bin/pip3 install -r /chiadog/requirements.txt
     fi
     echo 'Configuring Chiadog...'
