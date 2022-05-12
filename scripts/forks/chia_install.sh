7bac51dadf083fea99811c83171a806ab0a0a340
Guy Davis <guydavis.ca@gmail.com>
2022-05-12 13:49:30 -0600

    Switch to 'latest' branch to fix Chia versioning.

--- scripts/forks/chia_install.sh
+++ scripts/forks/chia_install.sh
@@ -4,16 +4,11 @@
 #
 
 CHIA_BRANCH=$1
-# On 2022-04-19
-HASH=d154105a6b35f94649f15bca4e3fb8a11a39e70e
 
 if [ -z ${CHIA_BRANCH} ]; then
-	echo 'Skipping Chia install as not requested.'
+    echo 'Skipping Chia install as not requested.'
 else
-	git clone --branch ${CHIA_BRANCH} --recurse-submodules https://github.com/Chia-Network/chia-blockchain.git /chia-blockchain
-	cd /chia-blockchain
-	git submodule update --init mozilla-ca 
-	git checkout $HASH
-	chmod +x install.sh
-	/usr/bin/sh ./install.sh
+    git clone --branch ${CHIA_BRANCH} --recurse-submodules=mozilla-ca https://github.com/Chia-Network/chia-blockchain.git /chia-blockchain
+    cd /chia-blockchain
+    /bin/sh ./install.sh
 fi
