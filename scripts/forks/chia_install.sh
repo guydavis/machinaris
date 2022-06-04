cc90e134e2fdd027357539a2a312021435c0fa6d
joshpainter <josh@joshpainter.com>
2022-06-03 19:00:50 -0500

    Revert to 0.7.1 code but update hash for Chia 1.3.5

--- scripts/forks/chia_install.sh
+++ scripts/forks/chia_install.sh
@@ -4,11 +4,14 @@
 #
 
 CHIA_BRANCH=$1
-
+HASH=0f5a6df4ffcd7b1d5b950b9f40c15b4e6045ee1b
 if [ -z ${CHIA_BRANCH} ]; then
     echo 'Skipping Chia install as not requested.'
 else
-    git clone --branch ${CHIA_BRANCH} --recurse-submodules=mozilla-ca https://github.com/Chia-Network/chia-blockchain.git /chia-blockchain
-    cd /chia-blockchain
-    /bin/sh ./install.sh
+	git clone --branch ${CHIA_BRANCH} --recurse-submodules https://github.com/Chia-Network/chia-blockchain.git /chia-blockchain
+	cd /chia-blockchain
+	git submodule update --init mozilla-ca 
+	git checkout $HASH
+	chmod +x install.sh
+	/usr/bin/sh ./install.sh
 fi
