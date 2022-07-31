fee11588adb9fee1927d35c1d39a03365743bc97
Guy Davis <guydavis.ca@gmail.com>
2022-07-31 09:41:07 -0600

    Fix broken megacmd install.

--- scripts/megacmd_setup.sh
+++ scripts/megacmd_setup.sh
@@ -2,17 +2,16 @@
 #
 # Installs megacmd for scripted downloads of pre-synced blockchains, when requested
 #
-# https://github.com/meganz/MEGAcmd 
+# https://github.com/meganz/MEGAcmd ; requires Ubuntu 22.04
 #
 
 if [[ ${mode} == 'fullnode' ]] && [[ "${blockchain_db_download}" == 'true' ]] && [[ ! -f /usr/bin/mega-cmd ]]; then
 	arch_name="$(uname -m)"
 	echo "Installing megacmd on ${arch_name}..."
 	if [[ "${arch_name}" = "x86_64" ]]; then
-		echo 'deb https://mega.nz/linux/MEGAsync/xUbuntu_21.04/ ./' > /etc/apt/sources.list.d/mega-nz.list 
-		curl -fsSL https://mega.nz/keys/MEGA_signing.key | apt-key add - 
-		apt-get update
-		apt-get install -y megacmd
+		cd /tmp
+		curl -O https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megacmd_1.5.1-1.1_amd64.deb
+		apt-get install -y ./megacmd_1.5.1-1.1_amd64.deb
 	else
 		echo "Installing megacmd skipped -> unsupported architecture: ${arch_name}"
 	fi
