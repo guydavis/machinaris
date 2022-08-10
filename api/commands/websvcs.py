712d4ba2cdc09bfb6b5758cd5f975d9f52606669
Guy Davis <guydavis.ca@gmail.com>
2022-08-09 18:50:54 -0600

    Fixes for wallets with many, many transactions.

--- api/commands/websvcs.py
+++ api/commands/websvcs.py
@@ -15,6 +15,7 @@ from common.config import globals
 from api import app
 
 ALLTHEBLOCKS_REQUEST_INTERVAL_MINS = 15
+ALLTHEBLOCKS_MAX_TRANSACTION_PAGES = 20
 COLD_WALLET_ADDRESSES_FILE = '/root/.chia/machinaris/config/cold_wallet_addresses.json'
 COLD_WALLET_CACHE_FILE = '/root/.chia/machinaris/cache/cold_wallet_cache.json'
 COLD_WALLET_CACHE_DIR = '/root/.chia/machinaris/cache/cold_wallets'
@@ -54,6 +55,7 @@ def load_cold_wallet_cache():
     return data
 
 def save_cold_wallet_cache(cold_wallet_cache):
+    app.logger.info("SAVING COLD WALLET CACHE: {0}".format(cold_wallet_cache))
     try:
         with open(COLD_WALLET_CACHE_FILE, 'w') as f:
             json.dump(cold_wallet_cache, f)
@@ -78,7 +80,7 @@ def request_cold_wallet_transactions_page(alltheblocks_blockchain, address, page
         url += f"?pageNumber={page_num}"
     if page_size:
         url += f"&pageSize={page_size}"
-    response = json.loads(requests.get(url).content)
+    response = json.loads(requests.get(url, timeout=30).content)
     #app.logger.info(response)
     return [ response['number'], response['size'], response['totalPages'], response['content'] ]
 
@@ -92,7 +94,11 @@ def request_cold_wallet_transactions(blockchain, alltheblocks_blockchain, addres
         [page_num, page_size, total_pages, transactions] = request_cold_wallet_transactions_page(alltheblocks_blockchain, address, page_num, page_size)
         records.extend(transactions)
         if page_num >= total_pages:
-            break  # have all the pages of response
+            app.logger.info("Returning last page # {0} out of {1} for {2}".format(page_num, total_pages, blockchain))
+            break
+        elif page_num >= ALLTHEBLOCKS_MAX_TRANSACTION_PAGES:
+            app.logger.info("Returning only # {0} pages out of {1} for {2}".format(page_num, total_pages, blockchain))
+            break  # have all the pages of response, or as many as we're going to get
         else: # get the next page
             page_num += 1
     app.logger.info("Found {0} transactions for {1}: {2}.".format(len(records), blockchain, address))
@@ -120,56 +126,68 @@ def request_cold_wallet_balance(blockchain, cold_wallet_cache, alltheblocks_bloc
     try:
         if debug:
             http.client.HTTPConnection.debuglevel = 1
-        response = json.loads(requests.get(url).content)
+        response = json.loads(requests.get(url, timeout=30).content)
         if 'balance' in response:
             total_balance = response['balance'] / globals.get_mojos_per_coin(blockchain)
-            farmed_balance = request_cold_wallet_transactions(blockchain, alltheblocks_blockchain, address, debug)
-            app.logger.info("Received cold wallet total balance of {0} and farmed balance of {1}".format(total_balance, farmed_balance))
-            # Store summary results for this cold wallet address
-            cold_wallet_cache[address] = {
-                'total_balance': total_balance,
-                'farmed_balance': farmed_balance
-            }
+            app.logger.info("Received {0} cold wallet total balance of {1}".format(blockchain, total_balance))
         else:
             app.logger.error("Received malformed response from ATB: {0}".format(response))
     except Exception as ex:
-        app.logger.error("Failed to request {0} due to {1}".format(url, str(ex)))
-        traceback.print_exc()
+        app.logger.error("Failed to request cold wallet balance for {0} due to {1}".format(url, str(ex)))
     finally:
         http.client.HTTPConnection.debuglevel = 0
+    if total_balance > 0:
+        try:
+            farmed_balance = request_cold_wallet_transactions(blockchain, alltheblocks_blockchain, address, debug)
+            app.logger.info("Received {0} farmed wallet total balance of {1}".format(blockchain, farmed_balance))
+        except Exception as ex:
+            app.logger.error("Failed to request cold wallet transactions for {0} due to {1}".format(url, str(ex)))
+        finally:
+            http.client.HTTPConnection.debuglevel = 0
+    if total_balance > 0:
+        # Store summary results for this cold wallet address
+        cold_wallet_cache[address] = {
+            'total_balance': total_balance,
+            'farmed_balance': farmed_balance
+        }
+    app.logger.info("Returning cold wallet total balance: {0}".format(total_balance))
     return total_balance  # May be None if no good response received
 
 last_cold_wallet_request_time = None
 def cold_wallet_balance(blockchain):
     global last_cold_wallet_request_time
     total_balance = 0.0
-    alltheblocks_blockchain = globals.get_alltheblocks_name(blockchain)
     addresses_per_blockchain = load_cold_wallet_addresses()
     cold_wallet_cache = load_cold_wallet_cache()
-    if blockchain in addresses_per_blockchain:
-        if not last_cold_wallet_request_time or last_cold_wallet_request_time <= \
+    # First poll the web service, if it's been a while
+    if not last_cold_wallet_request_time or last_cold_wallet_request_time <= \
                 (datetime.datetime.now() - datetime.timedelta(minutes=ALLTHEBLOCKS_REQUEST_INTERVAL_MINS)):
-            for address in addresses_per_blockchain[blockchain]:
-                address_balance = request_cold_wallet_balance(blockchain, cold_wallet_cache, alltheblocks_blockchain, address)
-                if address_balance is not None:
-                    total_balance += address_balance
-                else:
-                    app.logger.error("No cold wallet details found for {0} {1}.".format(blockchain, address))
-            save_cold_wallet_cache(cold_wallet_cache)
-            last_cold_wallet_request_time = datetime.datetime.now()
-            return total_balance
-        else:
-            for address in addresses_per_blockchain[blockchain]:
-                if address in cold_wallet_cache:
-                    try:
-                        if 'total_balance' in cold_wallet_cache[address]:
-                            app.logger.debug(cold_wallet_cache[address])
-                            cached_cold_balance = float(cold_wallet_cache[address]['total_balance'])
-                            total_balance += cached_cold_balance
-                    except Exception as ex:
-                        app.logger.info("During cold balance lookup, deleting old cold wallet cache file with legacy format. {0}".format(str(ex)))
-                        os.remove(COLD_WALLET_CACHE_FILE)
-            return total_balance
+        last_cold_wallet_request_time = datetime.datetime.now()
+        for cold_blockchain in addresses_per_blockchain.keys():
+            try:
+                alltheblocks_blockchain = globals.get_alltheblocks_name(cold_blockchain)
+                for address in addresses_per_blockchain[cold_blockchain]:
+                    address_balance = request_cold_wallet_balance(cold_blockchain, cold_wallet_cache, alltheblocks_blockchain, address)
+                    if address_balance is not None:
+                        total_balance += address_balance
+                    else:
+                        app.logger.error("No cold wallet details found for {0} {1}.".format(cold_blockchain, address))
+            except:
+                app.logger.error("Failed to request cold wallet balance for {0} due to {1}".format(cold_blockchain, str(ex)))
+        save_cold_wallet_cache(cold_wallet_cache)
+    # Now for that specific blockchain, check for a cold wallet balance in the cache
+    if blockchain in addresses_per_blockchain:
+        for address in addresses_per_blockchain[blockchain]:
+            if address in cold_wallet_cache:
+                try:
+                    if 'total_balance' in cold_wallet_cache[address]:
+                        app.logger.debug(cold_wallet_cache[address])
+                        cached_cold_balance = float(cold_wallet_cache[address]['total_balance'])
+                        total_balance += cached_cold_balance
+                except Exception as ex:
+                    app.logger.info("During cold balance lookup, deleting old cold wallet cache file with legacy format. {0}".format(str(ex)))
+                    os.remove(COLD_WALLET_CACHE_FILE)
+        return total_balance
     return 0.0 # No cold wallet addresses to check, so no errors obviously
 
 def load_cold_wallet_transactions(blockchain, address):
@@ -238,7 +256,7 @@ def request_prices(prices, debug=False):
     app.logger.info("Requesting recent pricing for blockchains from {0}".format(url))
     if debug:
         http.client.HTTPConnection.debuglevel = 1
-    data = requests.get(url).text
+    data = requests.get(url, timeout=30).text
     http.client.HTTPConnection.debuglevel = 0
     soup = bs4.BeautifulSoup(data, 'html.parser')
     table = soup.find('table', class_="table b-table table-sm")
@@ -266,7 +284,7 @@ def request_peers(blockchain, debug=False):
     app.logger.info("Requesting node peers for {0} from {1}".format(blockchain, url))
     if debug:
         http.client.HTTPConnection.debuglevel = 1
-    data = requests.get(url).text
+    data = requests.get(url, timeout=30).text
     http.client.HTTPConnection.debuglevel = 0
     soup = bs4.BeautifulSoup(data, 'html.parser')
     div = soup.find('div', class_="p-2 text-monospace")
@@ -286,7 +304,7 @@ def request_prices_api(prices, debug=False):
     app.logger.info("Requesting recent pricing for blockchains from {0}".format(url))
     if debug:
         http.client.HTTPConnection.debuglevel = 1
-    data = json.loads(requests.get(url).content)
+    data = json.loads(requests.get(url, timeout=30).content)
     http.client.HTTPConnection.debuglevel = 0
     for coin in data:
         app.logger.info("{0} @ {1}".format(coin['displayName'].lower(),coin['stats']['priceUsd']))
@@ -320,7 +338,7 @@ def save_exchange_rates(debug=False):
     try:
         if debug:
             http.client.HTTPConnection.debuglevel = 1
-        resp = requests.get(url)
+        resp = requests.get(url, timeout=30)
         http.client.HTTPConnection.debuglevel = 0
         if resp.status_code == 200:
             data = json.loads(resp.text)
