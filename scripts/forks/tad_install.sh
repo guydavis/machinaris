9df1b63e09adae6810d107b690bffe8854bf62d7
Guy Davis <guydavis.ca@gmail.com>
2022-08-10 08:48:56 -0600

    Update to latest Tad

--- scripts/forks/tad_install.sh
+++ scripts/forks/tad_install.sh
@@ -1,25 +1,28 @@
 #!/bin/env bash
 #
-# Installs Tad as per https://github.com/Tad-Network/tad-blockchain
+# Installs Tad as per https://github.com/BTCgreen-Network/tad-blockchain
 #
 
 TAD_BRANCH=$1
-# On 2022-08-07
-HASH=5fb43935cb741a820a1c10470cd6bff552e1a161
+# On 2022-08-010
+HASH=80ccd218e21f71450420c88c4158bf3a43571caa
 
 if [ -z ${TAD_BRANCH} ]; then
 	echo 'Skipping Tad install as not requested.'
 else
 	rm -rf /root/.cache
-	git clone --branch ${TAD_BRANCH} --single-branch https://github.com/Tad-Network/tad-blockchain.git /tad-blockchain
+	git clone --branch ${TAD_BRANCH} --single-branch https://github.com/BTCgreen-Network/tad-blockchain.git /tad-blockchain
 	cd /tad-blockchain 
 	git submodule update --init mozilla-ca 
 	git checkout $HASH
 	chmod +x install.sh
-	# 2022-01-30: pip broke due to https://github.com/pypa/pip/issues/10825
-	sed -i 's/upgrade\ pip$/upgrade\ "pip<22.0"/' install.sh
+
+    # Log "Added Coins" at info, not debug level.  See: https://github.com/Chia-Network/chia-blockchain/issues/11955
+    sed -i -e 's/^        self.log.debug($/        self.log.info(/g' tad/wallet/wallet_state_manager.py
+
 	# 2022-07-20: Python needs 'packaging==21.3'
 	sed -i 's/packaging==21.0/packaging==21.3/g' setup.py
+
 	/usr/bin/sh ./install.sh
 
 	if [ ! -d /chia-blockchain/venv ]; then
