62855106d19aeed377397b3711eeea6fe8f6b5c5
Guy Davis <guydavis.ca@gmail.com>
2022-09-20 09:34:09 -0600

    More fdccli update testing.

--- scripts/fd-cli_setup.sh
+++ scripts/fd-cli_setup.sh
@@ -5,9 +5,6 @@
 # Not needed on either the Chia or Chives images, only other forks.
 #
 
-# On 2021-11-08
-HASH=c9f562728acde289d3406d5cdc5a1139fd4267c8
-
 FDCLI_BRANCH=$1
 
 if [[ ${mode} == 'fullnode' ]]; then
@@ -15,7 +12,6 @@ if [[ ${mode} == 'fullnode' ]]; then
         cd /
         git clone --branch ${FDCLI_BRANCH} https://github.com/guydavis/flora-dev-cli.git
         cd flora-dev-cli
-        git checkout $HASH
         pip install -e . --extra-index-url https://pypi.chia.net/simple/
     fi
 fi
