a84065c5b43ba37b4b013139f72f8699e6fdd25d
Guy Davis <guydavis.ca@gmail.com>
2022-12-07 21:03:09 -0700

    Use a newer AMD GPU driver for MMX.

--- scripts/forks/mmx_install.sh
+++ scripts/forks/mmx_install.sh
@@ -14,8 +14,8 @@ else
 	apt-get install -y git cmake build-essential libsecp256k1-dev libsodium-dev zlib1g-dev ocl-icd-opencl-dev clinfo screen
 	apt-get install -y initramfs-tools ocl-icd-libopencl1 opencl-headers apt-utils libnuma1
 	# For AMDGPU, install the amdgpu-install stub, optionally invoked later if OPENCL_GPU=amd at launch time
-	curl -O http://repo.radeon.com/amdgpu-install/21.40/ubuntu/focal/amdgpu-install-21.40.40500-1_all.deb
-	apt install -y ./amdgpu-install-21.40.40500-1_all.deb
+	curl -O http://repo.radeon.com/amdgpu-install/22.20.5/ubuntu/jammy/amdgpu-install_22.20.50205-1_all.deb
+	apt-get install -y ./amdgpu-install_22.20.50205-1_all.deb
 	# Clone and install MMX
 	git clone --branch ${MMX_BRANCH} --single-branch https://github.com/madMAx43v3r/mmx-node.git /mmx-node
 	cd /mmx-node
