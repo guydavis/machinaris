247dea90b31bf18b291744ae3bb21bc09ca9dfef
Guy Davis <guydavis.ca@gmail.com>
2023-01-13 16:59:58 -0700

    Start using Max's MMX binaries.

--- scripts/forks/mmx_install.sh
+++ scripts/forks/mmx_install.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 #
 # Installs MMX as per https://github.com/madMAx43v3r/mmx-node
+# using his binaries from https://github.com/madMAx43v3r/mmx-binaries.git
 #
 
 MMX_BRANCH=$1
@@ -16,9 +17,16 @@ else
 	# For AMDGPU, install the amdgpu-install stub, optionally invoked later if OPENCL_GPU=amd at launch time
 	curl -O http://repo.radeon.com/amdgpu-install/22.20.5/ubuntu/jammy/amdgpu-install_22.20.50205-1_all.deb
 	apt-get install -y ./amdgpu-install_22.20.50205-1_all.deb
-	# Clone and install MMX
-	git clone --branch ${MMX_BRANCH} --single-branch https://github.com/madMAx43v3r/mmx-node.git /mmx-node
-	cd /mmx-node
-	git submodule update --init --recursive 
-	./make_release.sh
+	# Clone and install MMX from the author's own binaries, not linked to his code branches unfortunately
+	pushd /tmp
+	git clone --depth 1 --filter=blob:none --sparse https://github.com/madMAx43v3r/mmx-binaries.git
+	pushd mmx-binaries/
+	git sparse-checkout set mmx-node/linux/x86_64/
+	pushd mmx-node/linux
+	mv x86_64 /mmx-node
+	popd
+	rm -f mmx-binaries
+	popd
+	ls -1 /
+	ls -1 /mmx-node/
 fi
