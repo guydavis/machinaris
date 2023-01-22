43d22d316e1b5fd3b8d268848a94f0de413c8785
Guy Davis <guydavis.ca@gmail.com>
2023-01-22 14:07:00 -0700

    Another try for GPU support.

--- scripts/forks/chia_install.sh
+++ scripts/forks/chia_install.sh
@@ -10,9 +10,8 @@ if [ -z ${CHIA_BRANCH} ]; then
 else
     rm -rf /root/.cache
 	apt-get update
-	# Install dependencies for MMX and GPU support
-	apt-get install -y git cmake build-essential libsecp256k1-dev libsodium-dev libminiupnpc-dev libjemalloc-dev zlib1g-dev ocl-icd-opencl-dev clinfo screen
-	apt-get install -y initramfs-tools ocl-icd-libopencl1 opencl-headers apt-utils libnuma1
+	# Install dependencies for GPU support
+	apt-get install -y git cmake build-essential ocl-icd-opencl-dev clinfo screen initramfs-tools ocl-icd-libopencl1 opencl-headers apt-utils
 	# For AMDGPU, install the amdgpu-install stub, optionally invoked later if OPENCL_GPU=amd at launch time
 	curl -O http://repo.radeon.com/amdgpu-install/22.20.5/ubuntu/jammy/amdgpu-install_22.20.50205-1_all.deb
 	apt-get install -y ./amdgpu-install_22.20.50205-1_all.deb
