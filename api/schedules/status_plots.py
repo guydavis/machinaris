d040aefef92385a92e0701ba0ec49dc5d323e334
Guy Davis <guydavis.ca@gmail.com>
2023-01-30 15:40:44 -0700

    Display compression level of plotting.

--- api/schedules/status_plots.py
+++ api/schedules/status_plots.py
@@ -29,7 +29,7 @@ last_full_send_time = None
 
 def get_plot_attrs(plot_id, filename):
     dir,file = os.path.split(filename)
-    match = re.match("plot(?:-mmx)?-k(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\w+).plot", file)
+    match = re.match("plot(?:-mmx)?-k(\d+)(-c\d)?-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\w+).plot", file)
     if match:
         short_plot_id = match.group(7)[:16]
         created_at = "{0}-{1}-{2} {3}:{4}".format( match.group(2),match.group(3),match.group(4),match.group(5),match.group(6))
