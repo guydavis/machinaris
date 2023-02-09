6c5a4562a52cb1529ff4a71e2943d6158523b1e3
Guy Davis <guydavis.ca@gmail.com>
2023-02-09 11:32:20 -0700

    Test latest Madmax plotters for Chia and Chives too.

--- scripts/madmax_setup.sh
+++ scripts/madmax_setup.sh
@@ -1,44 +1,13 @@
 #!/bin/env bash
 #
-# Installs chia-plotter (pipelined multi-threaded) from binaries
+# Installs Madmax plotters from his binary repository
 #
-
-# As of 2022-08-20, https://github.com/madMAx43v3r/chia-plotter
-HASH=d1a9e88b44ba37f61bfabcb68e80e83f8b939648
-ORIG_MADMAX_BRANCH=master
 
 # MMX Plotter binaries, https://github.com/madMAx43v3r/chia-gigahorse
 MADMAX_BRANCH=$1
 
-# Currently Chia and Chives get the "old" Madmax plotter (no compresssion), built from source
-if [[ (${mode} == 'fullnode' || ${mode} =~ "plotter") && (${blockchains} == 'chia' || ${blockchains} == 'chives') ]]; then
-    if [ ! -f /usr/bin/chia_plot ] && [[ "${madmax_skip_build}" != 'true' ]]; then
-        arch_name="$(uname -m)"
-        if [[ "${arch_name}" = "x86_64" ]] || [[ "${arch_name}" = "arm64" ]]; then
-            apt update && apt install -y libsodium-dev cmake g++ git build-essential
-            cd /
-            git clone --branch ${ORIG_MADMAX_BRANCH} https://github.com/madMAx43v3r/chia-plotter.git 
-            cd chia-plotter && echo "Building madmax on ${arch_name}..."
-            if [[ -z "${madmax_relic_main}" ]]; then  # Hack on 2021-11-29 due to failed builds on some systems...
-                sed -i 's/set(ENV{RELIC_MAIN} "1")/#set(ENV{RELIC_MAIN} "1")/g' CMakeLists.txt
-            fi
-            git submodule update --init
-            git checkout $HASH
-            ./make_devel.sh
-            mkdir -p /usr/lib/chia-plotter
-            cp -r ./build/* /usr/lib/chia-plotter
-            ln -s /usr/lib/chia-plotter/chia_plot /usr/bin/chia_plot
-            ln -s /usr/lib/chia-plotter/chia_plot_k34 /usr/bin/chia_plot_k34
-            cd /
-            rm -rf chia-plotter
-       else
-            echo "Building madmax skipped -> unsupported architecture: ${arch_name}"
-        fi
-    fi
-fi
-
 # MMX blockchain container gets the "new" Madmax plotters, with compression, only available as binaries
-if [[ (${mode} == 'fullnode' || ${mode} =~ "plotter") && (${blockchains} == 'mmx') ]]; then
+if [[ (${mode} == 'fullnode' || ${mode} =~ "plotter") && (${blockchains} == 'mmx' || ${blockchains} == 'chia' || ${blockchains} == 'chives') ]]; then
     if [ ! -f /usr/bin/chia_plot ] && [[ "${madmax_skip_build}" != 'true' ]]; then
         arch_name="$(uname -m)"
         if [[ "${arch_name}" = "x86_64" ]]; then
@@ -53,10 +22,13 @@ if [[ (${mode} == 'fullnode' || ${mode} =~ "plotter") && (${blockchains} == 'mmx
             curl -sLJO https://github.com/madMAx43v3r/chia-gigahorse/raw/${MADMAX_BRANCH}/cuda-plotter/linux/x86_64/cuda_plot_k32
             curl -sLJO https://github.com/madMAx43v3r/chia-gigahorse/raw/${MADMAX_BRANCH}/cuda-plotter/linux/x86_64/cuda_plot_k33
             chmod 755 cuda_plot*
+            curl -sLJO https://github.com/madMAx43v3r/chia-gigahorse/raw/${MADMAX_BRANCH}/chiapos/linux/x86_64/ProofOfSpace
+            chmod 755 ProofOfSpace
             popd
             echo "Completed download of Madmax binaries for plotting:"
             echo "chia_plot @ "`chia_plot --version`
             echo "cuda_plot @ "`cuda_plot_k32 --version`
+            echo "ProofOfSpace @ "`ProofOfSpace --version`
         else
             echo "Downloading MMX chia_plot and cuda_plot skipped -> unsupported architecture: ${arch_name}"
         fi
