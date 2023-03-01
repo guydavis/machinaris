c6670c1b89f67be5d618870e3d6155305ddbaa73
Guy Davis <guydavis.ca@gmail.com>
2023-03-01 16:44:32 -0700

    Need to activate.

--- scripts/timelord_setup.sh
+++ scripts/timelord_setup.sh
@@ -2,6 +2,8 @@
 # Optionally builds and starts a timelord if environment variable of `mode` is `fullnode,timelord`
 #
 
-echo 'Attempting to build timelord binaries for this blockchain...'
 apt update && apt install -y libgmp-dev libboost-python-dev libboost-system-dev
+echo 'Attempting to build timelord binaries for this blockchain...'
+cd /chia-blockchain
+. ./activate
 BUILD_VDF_CLIENT=Y BUILD_VDF_BENCH=Y /usr/bin/sh ./install-timelord.sh
