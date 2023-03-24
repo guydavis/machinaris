f89daa137d2aa705fcc5a838b66a0626ea2d19a1
Guy Davis <guydavis.ca@gmail.com>
2023-03-24 10:08:06 -0600

    Next development version: 0.8.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,16 @@
 
 All notable changes to this project will be documented in this file. The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+### Added
+ - 
+## Changed
+ -
+## Updated 
+ - 
+## Notes
+ - Support for new blockchains and tools DOES NOT imply my endorsement for them.  *Only run those you are comfortable with.*
+
 ## [0.8.8] - 2023-03-23
 ### Added
  - Support for running a blockchain timelord with environment variable `mode` set to `fullnode,timelord`.
--- scripts/forks/chia_install.sh
+++ scripts/forks/chia_install.sh
@@ -10,17 +10,23 @@ if [ -z ${CHIA_BRANCH} ]; then
 else
     cd /tmp
     rm -rf /root/.cache
-	apt-get update && apt-get install -y dialog apt-utils
+    apt-get update && apt-get install -y dialog apt-utils
     /usr/bin/bash /machinaris/scripts/gpu_drivers_install.sh
-	
+
     git clone --branch ${CHIA_BRANCH} --recurse-submodules=mozilla-ca https://github.com/Chia-Network/chia-blockchain.git /chia-blockchain
     cd /chia-blockchain
 
     # Log "Added Coins" at info, not debug level.  See: https://github.com/Chia-Network/chia-blockchain/issues/11955
     sed -i -e 's/^        self.log.debug($/        self.log.info(/g' chia/wallet/wallet_state_manager.py
 
-	# 2022-07-20: Python needs 'packaging==21.3'
-	sed -i 's/packaging==21.0/packaging==21.3/g' setup.py
+    # Now correct the version that Chia binaries report.  Use the actual version checked out from Github.
+    branch_version=$(echo $CHIA_BRANCH | grep -Po '(?<=release/)\d.\d.\d')
+    if [ $? -eq 0 ]; then # Will match if branch is "release/X.Y.Z", not if branch is "main" (development stream)
+        echo "Building Chia release version ${branch_version}"
+        sed -i "/name=\"chia-blockchain\",/a \ \ \ \ version=\"${branch_version}\"," setup.py
+    else
+        echo "Building Chia development version from branch ${CHIA_BRANCH}"
+    fi
 
     /bin/sh ./install.sh
 
