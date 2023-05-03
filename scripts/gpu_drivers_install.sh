e983386c1b772cebd73208ae9f666baa52965390
Guy Davis <guydavis.ca@gmail.com>
2023-05-03 10:01:28 -0600

    Pin AMD GPU drivers to 5.4 as new 5.5 is broken.

--- scripts/gpu_drivers_install.sh
+++ scripts/gpu_drivers_install.sh
@@ -11,7 +11,9 @@ echo "Installing GPU drivers on ${arch_name} for ${ubuntu_ver}..."
 apt update && apt install -y git cmake build-essential ocl-icd-opencl-dev clinfo screen initramfs-tools ocl-icd-libopencl1 opencl-headers libnuma1
 
 # For AMDGPU, install the amdgpu-install stub, optionally invoked later if OPENCL_GPU=amd at launch time
-amd_deb=$(curl -s http://repo.radeon.com/amdgpu-install/latest/ubuntu/jammy/ | grep -m 1 -Po "amdgpu-install_[-\.\d]+_all.deb" | head -1)
-curl -O http://repo.radeon.com/amdgpu-install/latest/ubuntu/jammy/${amd_deb}
+# 2023-05-30 http://repo.radeon.com/amdgpu-install/latest/ubuntu/jammy/ (5.5) is currently broken, pinning old version (5.4)
+driver_version=5.4  # Try 'latest' again once they fix it.
+amd_deb=$(curl -s http://repo.radeon.com/amdgpu-install/${driver_version}/ubuntu/jammy/ | grep -m 1 -Po "amdgpu-install_[-\.\d]+_all.deb" | head -1)
+curl -O http://repo.radeon.com/amdgpu-install/${driver_version}/ubuntu/jammy/${amd_deb}
 apt install -y ./${amd_deb}
 apt install -y radeontop
