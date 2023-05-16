9d97c8493fa7fb5daaf6aa62d36315d2dd74c737
Guy Davis <guydavis.ca@gmail.com>
2023-05-16 10:00:16 -0600

    Test out the cuda-enabled Chia farming binaries.

--- scripts/forks/chia_install.sh
+++ scripts/forks/chia_install.sh
@@ -13,12 +13,14 @@ else
     apt-get update && apt-get install -y dialog apt-utils
     /usr/bin/bash /machinaris/scripts/gpu_drivers_install.sh
 
-    git clone --branch ${CHIA_BRANCH} --recurse-submodules=mozilla-ca https://github.com/Chia-Network/chia-blockchain.git /chia-blockchain
-    cd /chia-blockchain
+    curl -sLJO https://download.chia.net/bladebit/alpha4/chia-blockchain-cuda/ubuntu/chia-blockchain-cli_1.8.1rc2-dev34-1_amd64.deb
+    apt-get install chia-blockchain*.deb
+    ls -al /chia-blockchain
 
+    # For testing of new GPU farming binaries, comment out build from source for now.
+    #git clone --branch ${CHIA_BRANCH} --recurse-submodules=mozilla-ca https://github.com/Chia-Network/chia-blockchain.git /chia-blockchain
+    #cd /chia-blockchain
     # Log "Added Coins" at info, not debug level.  See: https://github.com/Chia-Network/chia-blockchain/issues/11955
-    sed -i -e 's/^        self.log.debug($/        self.log.info(/g' chia/wallet/wallet_state_manager.py
-
-    /bin/sh ./install.sh
-
+    #sed -i -e 's/^        self.log.debug($/        self.log.info(/g' chia/wallet/wallet_state_manager.py
+    #/bin/sh ./install.sh
 fi
