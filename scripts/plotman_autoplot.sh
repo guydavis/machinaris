79c4b7ddc31358c4120cfa46e1ba667b5e1da59d
Guy Davis <guydavis.ca@gmail.com>
2023-08-21 16:23:24 -0600

    Support for AUTO_ARCHIVE on startup.

--- scripts/plotman_autoplot.sh
+++ scripts/plotman_autoplot.sh
@@ -1,11 +1,15 @@
 #!/bin/env bash
 #
-# Starts Plotman, when AUTO_PLOT enabled, on a Chia/Chives fullnode or a plotter instance
+# Starts Plotman, when AUTO_PLOT/AUTO_ARCHIVE are enabled, on a Chia/Chives/MMX/Gigahorse fullnode or a plotter instance
 #
 
 if [[ (${mode} =~ ^fullnode.*  || ${mode} =~ "plotter") && (${blockchains} == 'chia' || ${blockchains} == 'chives' || ${blockchains} == 'mmx' || ${blockchains} == 'gigahorse') ]]; then
     # Start plotting automatically if requested (not the default)
     if [ ${AUTO_PLOT,,} = "true" ]; then
         nohup plotman plot >> /root/.chia/plotman/logs/plotman.log 2>&1 &
+    fi
+    # Start archiving automatically if requested (not the default)
+    if [ ${AUTO_ARCHIVE,,} = "true" ]; then
+        nohup plotman archive >> /root/.chia/plotman/logs/archiver.log 2>&1 &
     fi
 fi
