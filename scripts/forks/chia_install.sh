fd6a4b1a3e90d3a9588047911f37647a88034947
Guy Davis <guydavis.ca@gmail.com>
2023-09-27 09:07:05 -0600

    Chia 2.1.0rc3

--- scripts/forks/chia_install.sh
+++ scripts/forks/chia_install.sh
@@ -27,10 +27,10 @@ else
     echo "Installing Chia CUDA binaries on ${arch_name}..."
     cd /tmp
     if [[ "${arch_name}" = "x86_64" ]]; then
-        curl -sLJO https://github.com/Chia-Network/chia-blockchain/releases/download/2.1.0-rc2/chia-blockchain-cli_2.1.0rc2-1_amd64.deb
+        curl -sLJO https://github.com/Chia-Network/chia-blockchain/releases/download/2.1.0-rc3/chia-blockchain-cli_2.1.0rc3-1_amd64.deb
         apt-get install ./chia-blockchain-cli*.deb
     elif [[ "${arch_name}" = "arm64" ]]; then
-        curl -sLJO https://github.com/Chia-Network/chia-blockchain/releases/download/2.1.0-rc2/chia-blockchain-cli_2.1.0rc2-1_arm64.deb
+        curl -sLJO https://github.com/Chia-Network/chia-blockchain/releases/download/2.1.0-rc3/chia-blockchain-cli_2.1.0rc3-1_arm64.deb
         apt-get install ./chia-blockchain-cli*.deb
     else
         echo "Installing Chia CUDA binaries skipped -> unsupported architecture: ${arch_name}"
