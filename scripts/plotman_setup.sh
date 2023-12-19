9622c0dd8391c361e8980495f0f6935fe4d439ee
Guy Davis <guydavis.ca@gmail.com>
2023-12-19 15:00:17 -0700

    Fix for broken Plotman dependencies.

--- scripts/plotman_setup.sh
+++ scripts/plotman_setup.sh
@@ -11,7 +11,13 @@ if [[ (${mode} =~ ^fullnode.*  || ${mode} =~ "plotter") && (${blockchains} == 'c
         cd /
         git clone --branch ${PLOTMAN_BRANCH} https://github.com/guydavis/plotman.git
         cd plotman
-        /chia-blockchain/venv/bin/python setup.py install
+        /chia-blockchain/venv/bin/pip install .
+        # Workaround for broken dependency ordering
+        /chia-blockchain/venv/bin/pip install pendulum~=2.1
+        /chia-blockchain/venv/bin/pip install packaging~=21.0
+        /chia-blockchain/venv/bin/pip install attrs~=21.2
+        /chia-blockchain/venv/bin/pip install desert~=2020.11.18
+        /chia-blockchain/venv/bin/pip install appdirs~=1.4
         apt update && apt install -y rsync
         mkdir -p /root/.chia/plotman/logs/archiving
         tee /etc/logrotate.d/plotman >/dev/null <<EOF
