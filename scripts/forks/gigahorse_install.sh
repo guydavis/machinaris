80b07b6a27ce85c35baf63dbe1fcb87a94890e53
Guy Davis <guydavis.ca@gmail.com>
2023-12-23 13:40:19 -0700

    Underscore, not hyphen.

--- scripts/forks/gigahorse_install.sh
+++ scripts/forks/gigahorse_install.sh
@@ -16,7 +16,7 @@ else
 	
     arch_name="$(uname -m)"
     if [[ "${arch_name}" = "x86_64" ]]; then
-        url="https://github.com/madMAx43v3r/chia-gigahorse/releases/download/${GIGAHORSE_BRANCH}/chia-gigahorse-farmer-${GIGAHORSE_VERSION}-x86-64.tar.gz"
+        url="https://github.com/madMAx43v3r/chia-gigahorse/releases/download/${GIGAHORSE_BRANCH}/chia-gigahorse-farmer-${GIGAHORSE_VERSION}-x86_64.tar.gz"
     else
         url="https://github.com/madMAx43v3r/chia-gigahorse/releases/download/${GIGAHORSE_BRANCH}/chia-gigahorse-farmer-${GIGAHORSE_VERSION}-aarch64.tar.gz"
     fi
