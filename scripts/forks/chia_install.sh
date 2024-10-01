291f00f712ce783255eb10fbd83a8ccaedb11401
Guy Davis <guydavis.ca@gmail.com>
2024-10-01 16:46:58 -0600

    Chia @ v2.4.4 rc1

--- scripts/forks/chia_install.sh
+++ scripts/forks/chia_install.sh
@@ -27,10 +27,10 @@ else
     echo "Installing Chia CUDA binaries on ${arch_name}..."
     cd /tmp
     if [[ "${arch_name}" == "x86_64" ]]; then
-        curl -sLJO https://github.com/Chia-Network/chia-blockchain/releases/download/2.4.3/chia-blockchain-cli_2.4.3-1_amd64.deb
+        curl -sLJO https://github.com/Chia-Network/chia-blockchain/releases/download/2.4.4-rc1/chia-blockchain-cli_2.4.4-rc1-1_amd64.deb
         apt-get install ./chia-blockchain-cli*.deb
     else
-        curl -sLJO https://github.com/Chia-Network/chia-blockchain/releases/download/2.4.3/chia-blockchain-cli_2.4.3-1_arm64.deb
+        curl -sLJO https://github.com/Chia-Network/chia-blockchain/releases/download/2.4.4-rc1/chia-blockchain-cli_2.4.4-rc1-1_arm64.deb
         apt-get install ./chia-blockchain-cli*.deb
     fi
 
